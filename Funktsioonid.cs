498bb0dc5871cadfd6d2b82f0df90ea4f9908fca
opilane <opilane@HP-CZC2349HTM>
2024-08-26 16:25:07 +0300

    Add project files.

---
+++ Funktsioonid.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SergachevTARpv23
+{
+    public class Funktsioonid
+    {
+       /* public static void Tere(string nimi)
+        {
+            Console.WriteLine("Tere, {0}", nimi);
+        }
+        public static int Liitmine(int arv1, int arv2)
+        {
+            return arv1 + arv2;
+        }
+       */
+        // Loo arvuta funktsioon, mis sõltub 3 parametrist: tehe, arv1, arv2. Kasuta if konstruktsioon. Tulemus kuva ekraanile.
+        public static double Arvuta(char tehe, double arv1, double arv2)
+        {
+            double tulemus;
+
+            if (tehe == '+')
+            {
+                tulemus = arv1 + arv2;
+            }
+            else if (tehe == '-')
+            {
+                tulemus = arv1 - arv2;
+            }
+            else if (tehe == '*')
+            {
+                tulemus = arv1 * arv2;
+            }
+            else if (tehe == '/')
+            {
+                if (arv2 != 0)
+                {
+                    tulemus = arv1 / arv2;
+                }
+                else
+                {
+                    Console.Error.WriteLine("Jagamine nulliga ei ole lubatud");
+                    return 0;
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine("Tundmatu tehe");
+                return 0;
+            }
+
+                Console.WriteLine($"Tulemus: {tulemus}");
+                return tulemus;
+            
+        }
+        public static string Jukku(int vanus)
+        {
+            string eesnimi = "Jukku";
+
+            if (eesnimi == "Juku")
+            {
+                if (vanus < 12)
+                {
+                    return "Ostame Jukule lastepileti.";
+                }
+                else if (vanus >= 12 && vanus < 18)
+                {
+                    return "Ostame Jukule noortepileti.";
+                }
+                else
+                {
+                    return "Ostame Jukule täispileti.";
+                }
+            } else { 
+
+                return "Eesnimi ei ole Juku.";
+            }
+        }
+    }
+}
+
---
+++ MainClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SergachevTARpv23
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+             Console.OutputEncoding = Encoding.UTF8;
+            /* Console.WriteLine("Hello World!!!");
+             string nimetus = "Python";
+             Console.WriteLine(nimetus);
+             Console.WriteLine("Hello, my friend, {0}", nimetus);
+             //Funktsioonid.Tere(nimetus);
+             Console.Write("Sisesta esimene arv: ");
+             int a = int.Parse(Console.ReadLine());
+             Console.Write('\n');
+             Console.Write("Sisesta teine arv: ");
+             int b = int.Parse(Console.ReadLine());
+             int vastus = Funktsioonid.Liitmine(a, b);
+             Console.Write('\n');
+             Console.WriteLine("Vastus on: {0}", vastus);
+             double arv = 5.123456;
+             vastus = Funktsioonid.Liitmine(a, (int)arv);
+             Console.Write("\n");
+             Console.WriteLine("Vastus on: {0}", vastus);
+             char taht = 'A';
+             if (vastus == 0)
+             {
+                 Console.Write('\n');
+                 Console.WriteLine(taht);
+             }
+             else 
+             { 
+                 Console.WriteLine(vastus);
+             }
+             */
+
+
+            // 
+            Console.Write("Sisesta sümbool (+, -, *, /): ");
+            char sumbool = char.Parse(Console.ReadLine());
+            Console.Write("\n");
+            Console.Write("Sisesta esimene arv: ");
+            double arv5 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n");
+            Console.Write("Sisesta teine arv: ");
+            double arv6 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n");
+            double vastu = Funktsioonid.Arvuta(sumbool, arv5, arv6);
+            Console.Write('\n');
+            Console.WriteLine("Vastu: {0}", vastu);
+
+            // JUKU KINNOS KAIMA 
+            Console.Write("Mis on sinu nimi?: ");
+            string eesnimi = Console.ReadLine();
+            Console.Write('\n');
+            Console.WriteLine("Kui vana sa oled?: ");
+            int vanus = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+            string kinno = Funktsioonid.Jukku(vanus);
+            Console.WriteLine(kinno);
+        }
+    }
+}
